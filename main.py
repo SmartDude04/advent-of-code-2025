dc4ee2b3116648045a63137811293303b3475078
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2025-12-01 19:16:38 -0500

    Update main to use command-line arguments

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import importlib
+import argparse
 
 day = "01"
-use_example = False
-part = 2
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Advent of Code 2025",
+        description="Run a solution to a 2025 Advent of Code problem.")
+    parser.add_argument("-e", "--example", action='store_true', help="Whether to use the example file.")
+    parser.add_argument("-p", "--part", default=1, help="Determines the part of the problem to run. Defaults to part 1.", type=int)
+    args = parser.parse_args()
+
+    if args.part < 1 or args.part > 2:
+        raise ValueError("Part must be 1 or 2.")
+
+    use_example = args.example
+    part = args.part
+
     file_name = f"solutions.day_{day}"
     program = importlib.import_module(file_name)
 
