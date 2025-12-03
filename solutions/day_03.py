1957edfd2ebd71d1311fe9ce2dd1b0395ca0e569
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2025-12-03 10:20:43 -0500

    Finish part 2

--- solutions/day_03.py
+++ solutions/day_03.py
@@ -24,4 +24,26 @@ def part_1(f: io.TextIOWrapper) -> int:
         joltage = 10 * int(max_first_num) + int(max_second_num)
         total_joltage += joltage
 
-    print(total_joltage)
+    return total_joltage
+
+
+def part_2(f: io.TextIOWrapper) -> int:
+    def highest_battery(cur: str, length: int) -> int:
+        # Same greedy algorithm as part 1, but with a varying battery length (12 in this case, but could be anything)
+        if length == 0:
+            return 0
+
+        # Choose this number
+        max_num, max_index = -1, -1
+        for i, num in enumerate(cur[:len(cur) - length + 1]):
+            num = int(num)
+            if num > max_num:
+                max_num, max_index = num, i
+        return 10**(length - 1) * max_num + highest_battery(cur[max_index+1:], length - 1)
+
+    total_joltage = 0
+    for line in f.readlines():
+        joltage = highest_battery(line.replace("\n", ""), 12)
+        total_joltage += joltage
+
+    return total_joltage
